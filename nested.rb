627e23f1b4e3054cf1c9fe669c8663a34649ab5f
Pancho Labrador <plabradorjr@gmail.com>
2019-10-14 05:21:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages]
+		 string = programmer_hash[:dennis_ritchie][:languages]
+		 string.to_s
 end
 
 def adding_matz
